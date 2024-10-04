af78667a36ef9b2f303a524dfa74fb5174e4bec4
czamora04 <carlos.zamora04@gmail.com>
2024-10-03 22:54:43 -0600

    Initial commit

---
+++ src/main/java/com/tiendaRunning/TiendaRunningApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaRunning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaRunningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaRunningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaRunning/TiendaRunningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaRunning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaRunningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
